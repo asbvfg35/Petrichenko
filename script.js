cb0fbdfa371a5104b7b04bc869783079d8e558ee
Andrey <asbvfg35@gmail.com>
2022-09-02 17:13:35 +0400

    First

---
+++ script.js
@@ -0,0 +1,21 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+const a = prompt('Один из последних просмотренных фильмов', '');
+const b = prompt('На сколько оцените его', '');
+const c = prompt('Один из последних просмотренных фильмов', '');
+const d = prompt('На сколько оцените его', '');
+
+
+
+
+const personalMoovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+  movies: {
+    a: b,
+    c: d
+  }
+};
